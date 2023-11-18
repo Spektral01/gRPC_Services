bdec761db43f480cbbf09e3c2e83eba76f8bb772
Spektral01 <chalyhgrisha01@gmail.com>
2023-11-18 13:53:13 +0700

    gRPC add

---
+++ GrpcService/GrpcService/Services/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Services
+{
+    public class CalculatorServiceImpl : Calculator.CalculatorBase
+    {
+        private readonly ILogger<CalculatorServiceImpl> _logger;
+
+        public CalculatorServiceImpl(ILogger<CalculatorServiceImpl> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<MultiplyResponse> Multiply(MultiplyRequest request, ServerCallContext context)
+        {
+            int result = request.Number1 * request.Number2;
+            _logger.LogInformation($"Received request to multiply {request.Number1} and {request.Number2}. Result: {result}");
+            return Task.FromResult(new MultiplyResponse { Result = result });
+        }
+    }
+}
---
+++ gRPC Client/gRPC Client/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Net.Client;
+using gRPC_Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace gRPC_Client
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7002");
+
+            var client = new Calculator.CalculatorClient(channel);
+            var request = new MultiplyRequest
+            {
+                Number1 = (int)numericUpDown1.Value,
+                Number2 = (int)numericUpDown2.Value
+            };
+
+            var response = client.Multiply(request);
+
+            label1.Text = response.Result.ToString();
+
+        }
+    }
+}
